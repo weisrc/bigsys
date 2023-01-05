3d0ebbe4fe65332e4f73ef78290e4f6896acbcde
weisrc <weihengjiang64@hotmail.com>
2023-01-05 15:14:42 -0500

    init

---
+++ intent_engine.py
@@ -0,0 +1,50 @@
+from typing import Callable, Dict, List, Tuple
+from transformers import pipeline
+from sentence_transformers import SentenceTransformer
+import torch
+
+st_model = SentenceTransformer('all-MiniLM-L6-v2')
+qa_model = pipeline("question-answering", model="deepset/roberta-base-squad2",
+                    tokenizer="deepset/roberta-base-squad2")
+
+INTENT_TYPE = Tuple[torch.Tensor, Dict[str, str], Callable]
+
+
+class IntentEngine:
+    def __init__(self):
+        self.intents: List[INTENT_TYPE] = []
+
+    def intent(self, *texts, **questions):
+        def wrapper(func):
+            embeds = st_model.encode(texts, convert_to_tensor=True)
+            self.intents.append((embeds, questions, func))
+            return func
+        return wrapper
+
+    def predict(self, text):
+        text_embed = st_model.encode(text, convert_to_tensor=True)
+        best_score = 0
+        best_intent: INTENT_TYPE = None
+        for intent in self.intents:
+            for intent_embed in intent[0]:
+                score = intent_embed.dot(text_embed)
+                print(score)
+                if score > best_score:
+                    best_score = score
+                    best_intent = intent
+
+        if best_intent is None:
+            return
+
+        _, questions, func = best_intent
+        varnames = func.__code__.co_varnames[:func.__code__.co_argcount]
+        answers: Dict[str, str] = dict()
+        answer_scores: Dict[str, float] = dict()
+        for varname in varnames:
+            question = questions.get(varname)
+            if question is None:
+                continue
+            qa_out = qa_model(question=question, context=text)
+            answers[varname] = qa_out['answer']
+            answer_scores[varname] = qa_out['score']
+        return func, answers, best_score, answer_scores, questions
---
+++ main.py
@@ -0,0 +1,67 @@
+import os
+import discord
+# from detoxify import Detoxify
+import torch
+from intent_engine import IntentEngine
+import random
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+# model = Detoxify('original', device=os.environ.get('TORCH_DEVICE', DEVICE))
+client = discord.Client(intents=discord.Intents.all())
+
+ie = IntentEngine()
+
+@ie.intent('say hello to someone', 'greet someone', name='Who are you greeting?')
+async def say_hello(message: discord.Message, name: str):
+    await message.reply(f'Hello {name}!')
+
+
+@ie.intent('what the sum of two numbers', a='first number', b='second number')
+async def say_hello(message: discord.Message, a: str, b: str):
+    await message.reply(f'{a} + {b} = {int(a) + int(b)}')
+
+@ie.intent('what the difference of two numbers', a='first number', b='second number')
+async def say_hello(message: discord.Message, a: str, b: str):
+    await message.reply(f'{a} - {b} = {int(a) - int(b)}')
+
+@ie.intent('please roll a dice', faces='number of faces')
+async def roll_dice(message: discord.Message, faces):
+    n = int(faces)
+    await message.reply(f'You rolled a {random.randint(1, n)} from a {n}-sided dice!')
+
+@ie.intent('am I something', x='what are you')
+async def roll_dice(message: discord.Message, x):
+    await message.reply(f'Yes, you are {x}!')
+
+@ie.intent('thank you', x='for what reason')
+async def roll_dice(message: discord.Message, x):
+    await message.reply(f'No problem! I am glad I could help you with {x}!')
+
+@ie.intent('I hate you', x='for what reason')
+async def roll_dice(message: discord.Message, x):
+    await message.reply(f'Sorry for {x}, I will try to improve myself!')
+
+@client.event
+async def on_ready():
+    print(f'Logged on as {client.user}!')
+
+
+@client.event
+async def on_message(message: discord.Message):
+    if message.author.bot:
+        return
+    if message.author == client.user:
+        return
+    if message.mentions.count(client.user) == 0:
+        return
+    func, answers, score, answer_scores, questions = ie.predict(message.content)
+    print(score, answer_scores)
+    if score < 0.5:
+         await message.reply(f'I am not sure how to answer that. Sorry~ BUT I will give it a try even with a score as low as {score}')
+    for k, a_score in answer_scores.items():
+        if a_score < 0.5:
+            await message.reply(f"[score={a_score}] Can you please specify the following in your prompt: {questions[k]}")
+    await func(message, **answers)
+    
+
+client.run(os.environ['DISCORD_TOKEN'])

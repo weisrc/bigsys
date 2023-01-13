04f99009b65a3996dd9fb562c240e83dca40086b
weisrc <weihengjiang64@hotmail.com>
2023-01-12 21:08:30 -0500

    fix indent

--- bootstraps/command_bootstrap.py
+++ bootstraps/command_bootstrap.py
@@ -6,23 +6,23 @@ from handlers.command_handler import engine, command_handler
 
 
 def command_bootstrap():
-  engine.add('greet', name='.+')(greet)
-  engine.add('exit', 'e')(exit_vc)
-  engine.add('wiki', 'w', 'search', search='.+')(wiki)
+    engine.add('greet', name='.+')(greet)
+    engine.add('exit', 'e')(exit_vc)
+    engine.add('wiki', 'w', 'search', search='.+')(wiki)
 
-  engine.add('play', 'p', search='.+')(play_music)
-  engine.add('next', 'n', 'skip')(next_music)
-  engine.add('pause', 'stop', 's')(pause_music)
-  engine.add('resume', 'r', 'continue', 'c')(resume_music)
-  engine.add('list', 'l', 'queue', 'q')(list_music)
+    engine.add('play', 'p', search='.+')(play_music)
+    engine.add('next', 'n', 'skip')(next_music)
+    engine.add('pause', 'stop', 's')(pause_music)
+    engine.add('resume', 'r', 'continue', 'c')(resume_music)
+    engine.add('list', 'l', 'queue', 'q')(list_music)
 
-  engine.add('volume', 'vol', 'v', volume='\d+')(music_volume)
+    engine.add('volume', 'vol', 'v', volume='\d+')(music_volume)
 
-  engine.add('assistant', 'a')(start_assistant)
-  engine.add('noa', '-a')(stop_assistant)
+    engine.add('assistant', 'a')(start_assistant)
+    engine.add('noa', '-a')(stop_assistant)
 
-  engine.add('info', 'i')(info_creator)
-  engine.add('name', 'n')(info_name)
-  engine.add('functions', 'func', 'f')(info_functions)
-  
-  return command_handler
+    engine.add('info', 'i')(info_creator)
+    engine.add('name', 'n')(info_name)
+    engine.add('functions', 'func', 'f')(info_functions)
+
+    return command_handler

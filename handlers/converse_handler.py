564bc91493f450e18e3aae8ca5e5e0cffcb36a8b
weisrc <weihengjiang64@hotmail.com>
2023-03-08 21:00:28 -0500

    remove name replacement for conversational

--- handlers/converse_handler.py
+++ handlers/converse_handler.py
@@ -16,6 +16,5 @@ async def converse_handler(ctx: Context, _next):
     conversation.add_user_input(ctx.content)
     await execute(converse, conversation)
     response = conversation.generated_responses[-1]
-    response = response.replace('Sarah', ctx.client.user.name.capitalize())
     conversation.generated_responses[-1] = response
     await ctx.reply(response)
